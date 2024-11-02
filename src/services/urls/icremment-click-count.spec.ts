f724cc66854497873cfa7d601ee7421dcbd9b2d1
Willinghan Tomaz <willslaqtomaz@gmail.com>
2024-11-02 01:20:07 -0300

    test(url): added test to the delete on incremment click count

--- src/services/urls/icremment-click-count.spec.ts
+++ src/services/urls/icremment-click-count.spec.ts
@@ -2,14 +2,20 @@ import { UrlsRepository } from "@/repositories/urls.repository";
 import { beforeEach, describe, expect, it } from "vitest";
 import { CreateUrlService } from "./create-url.service";
 import { InMemoryUrlsRepository } from "@/repositories/in-memory/in-memory-urls-repository";
+import { ResourceNotFoundError } from "../errors/resource-not-found-error";
+import { IncremmentClickCountService } from "./icremment-click-count.service";
 
 let urlsRepository: UrlsRepository;
 let createUrlService: CreateUrlService;
+let incrementClickCountService: IncremmentClickCountService;
 
 describe("Increment Click Count Service", () => {
   beforeEach(() => {
     urlsRepository = new InMemoryUrlsRepository();
     createUrlService = new CreateUrlService(urlsRepository);
+    incrementClickCountService = new IncremmentClickCountService(
+      urlsRepository
+    );
   });
 
   it("should be able to increment click count", async () => {
@@ -19,8 +25,34 @@ describe("Increment Click Count Service", () => {
       userId: "user-01",
     });
 
-    const updatedUrl = await urlsRepository.incrementVisits(url.shorten_url);
+    const { url: updatedUrl } = await incrementClickCountService.execute({
+      shortenUrl: url.shorten_url,
+    });
 
     expect(updatedUrl.click_count).toEqual(1);
+  });
+
+  it("should not be able to increment click count if url not found", async () => {
+    await expect(() =>
+      incrementClickCountService.execute({
+        shortenUrl: "non-existing-url",
+      })
+    ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it("should not be able to increment click count if url already deleted", async () => {
+    const { url } = await createUrlService.execute({
+      expirationDate: new Date(),
+      originalUrl: "https://google.com.br",
+      userId: "user-01",
+    });
+
+    await urlsRepository.deleteById(url.id);
+
+    await expect(() =>
+      incrementClickCountService.execute({
+        shortenUrl: url.shorten_url,
+      })
+    ).rejects.toBeInstanceOf(ResourceNotFoundError);
   });
 });
