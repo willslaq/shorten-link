1057cba013d35e1a2cb23a90bf0e2b1623bef880
Willinghan Tomaz <willslaqtomaz@gmail.com>
2024-11-02 11:11:36 -0300

    feat(user): addded the route to refresh token

--- src/http/controllers/routes.ts
+++ src/http/controllers/routes.ts
@@ -1,9 +1,11 @@
 import { FastifyInstance } from "fastify";
 import { registerController } from "./register.controller";
 import { authenticateController } from "./authenticate.controller";
+import { refreshTokenController } from "./refresh.controller";
 
 export async function userRoutes(app: FastifyInstance) {
   app.post("/users", registerController);
   app.post("/sessions", authenticateController);
-  
+
+  app.patch('/token/refresh', refreshTokenController)
 }
