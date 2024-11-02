fac0d6a461da79e9c5282b7bc55a2f6b07824a6d
Willinghan Tomaz <willslaqtomaz@gmail.com>
2024-11-02 11:29:20 -0300

    chore: added e2e tests envrionment config

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    environment: "prisma",
+    setupFiles: "./prisma/vitest-environment-prisma/prisma-test-environment.ts",
+    environmentMatchGlobs: [["src/http/controllers/**", "prisma"]],
+    dir: "src",
+  },
 });
